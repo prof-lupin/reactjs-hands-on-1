6571a76d4adeb2d42afa79c7d01bf8a12b41025b
arya.shrivastava <arya.shrivastava@kollie.ai>
2020-10-02 13:29:17 +0530

    modified Details page and added Carousel implementation

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,50 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  //   constructor(props) {
+  //     super();
+
+  //     this.state = {
+  //       loading: true,
+  //     };
+  //   }
+
+  state = { loading: true };
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        species: animal.type,
+        location: `{${animal.contact.address.city}, ${animal.contact.address.state}}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { species, name, location, description, breed, media } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${species} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
